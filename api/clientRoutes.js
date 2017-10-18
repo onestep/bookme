7762ff40bd52e57609931f531f87f46608cfb31c
Valerii Liasotskyi <onestep@ukr.net>
2017-10-18 19:41:08 +0300

    Added REST API and DAO stubs

---
+++ api/clientRoutes.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var serviceController = require("./controllers/serviceController");
+
+/**
+ * @param {Object} app
+ */
+exports.init = function (app) {
+    app.route("/api/serviceGroups")
+        .get(serviceController.readRootServiceGroups);
+    app.route("/api/serviceGroups/:groupId")
+        .get(serviceController.readServiceGroups);
+    app.route("/api/services/:groupId")
+        .get(serviceController.readServices);
+};
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 "use strict";
 
 var express = require("express"),
+    clientRoutes = require("./api/clientRoutes"),
     app = express();
+
+clientRoutes.init(app);
 
 app.listen(8080);
