5ab2ecd26fa9abd0762757be0d2f98f1f6bd4bc7
Valerii Liasotskyi <onestep@ukr.net>
2017-10-22 00:05:37 +0300

    Added static content handling, added start web page

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var express = require("express"),
     clientRoutes = require("./api/clientRoutes"),
     app = express();
 
+app.use(express.static(__dirname + "/public"));
+
 clientRoutes.init(app);
 
 app.listen(8080);
