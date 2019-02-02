11706fc0a011110d3a041825cadc83530c13659a
Valerii Liasotskyi <onestep@ukr.net>
2019-02-02 17:58:26 +0200

    Added Mixpanel telemetry tracking for client front-end

--- public/js/src/module.js
+++ public/js/src/module.js
@@ -1,3 +1,26 @@
+mixpanel.init("de2c52c12dccdc9bc73a5949b888672b");
+
+function MixPanelInterceptorFactory() {
+    return {
+        request: function (config) {
+            mixpanel.track("Request started", {
+                method: config.method,
+                url: config.url
+            });
+            return config;
+        },
+        response: function (response) {
+            mixpanel.track("Request finished", {
+                method: response.config.method,
+                url: response.config.url,
+                status: response.status,
+                statusText: response.statusText
+            });
+            return response;
+        }
+    }
+}
+
 function ServiceGroupService($http) {
     this.$http = $http;
 }
@@ -17,5 +40,9 @@ LandingController.prototype.selectService = function () {
 };
 
 angular.module("reserveApp", ["ngRoute"])
+    .factory("mixPanelInterceptorFactory", MixPanelInterceptorFactory)
     .service("serviceGroupService", ServiceGroupService)
-    .controller("landingController", LandingController);
+    .controller("landingController", LandingController)
+    .config(function ($httpProvider) {
+        $httpProvider.interceptors.push("mixPanelInterceptorFactory");
+    });
